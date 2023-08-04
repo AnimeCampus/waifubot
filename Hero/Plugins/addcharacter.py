444d0efb77b64a9b90389e7338968555c7ce542d
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-08-04 23:58:25 +0530

    Update addcharacter.py

--- Hero/Plugins/addcharacter.py
+++ Hero/Plugins/addcharacter.py
@@ -3,12 +3,12 @@ import asyncio
 import random
 from pyrogram import Client, filters
 from Hero import pbot
-from Hero.database.uploaddb import upload_waifu
+from Hero.database.uploaddb import upload_waifu, upload_anime, upload_husbando
 import requests
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
-@Client.on_message(filters.command("addwaifu"))
+@Client.on_message(filters.command("addw"))
 async def add_waifu_characater(client, message):
     try:
         text = message.text.split(None, 1)[1]
@@ -18,6 +18,37 @@ async def add_waifu_characater(client, message):
         waifu_id = splitted[2]
         anime_id = splitted[3]
     except:
-        return await message.reply_text("format:\n`/addwaifu (url) (name) (waifu id) (anime id)`")
+        return await message.reply_text("format:\n`/addw (url) (name) (waifu id) (anime id)`")
     
     upload_waifu(url, waifu_id, name, anime_id)
+    await message.reply_text(f"Done!!\nID: {waifu_id}\nName: {waifu_name}\nUrl: `{url}`\nAnime ID: {anime_id}")
+
+
+@Client.on_message(filters.command("addh"))
+async def add_husbando_characater(client, message):
+    try:
+        text = message.text.split(None, 1)[1]
+        splitted = text.split(" ")
+        url = splitted[0]
+        name = splitted[1]
+        waifu_id = splitted[2]
+        anime_id = splitted[3]
+    except:
+        return await message.reply_text("format:\n`/addh (url) (name) (waifu id) (anime id)`")
+    
+    upload_husbando(url, waifu_id, name, anime_id)
+    await message.reply_text(f"Done!!\nID: {waifu_id}\nName: {waifu_name}\nUrl: `{url}`\nAnime ID: {anime_id}")
+
+
+@Client.on_message(filters.command("addanime"))
+async def add_anime_characater(client, message):
+    try:
+        text = message.text.split(None, 1)[1]
+        splitted = text.split(" ")
+        anime_id = splitted[0]
+        name = splitted[1]
+    except:
+        return await message.reply_text("format:\n`/addanime (anime id) (name)`")
+    
+    upload_anime(anime_id, name)
+    await message.reply_text(f"Done!!\nID: {waifu_id}\nName: {waifu_name}\nUrl: `{url}`\nAnime ID: {anime_id}")
