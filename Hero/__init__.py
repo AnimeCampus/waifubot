3de19c2c8a76e314e8e364aea26b65b87d693266
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-08-07 22:31:40 +0530

    Update __init__.py

--- Hero/__init__.py
+++ Hero/__init__.py
@@ -8,17 +8,17 @@ from pyrogram import Client
 
 API_ID = 14681826  # Your API ID
 API_HASH = "add59ab14dbbccf3c92c65ca4477f2fa"  # Your API Hash
-BOT_TOKEN = "6306432952:AAFe2CTLQDY7VVvuY9yRKm0FU7kXTgdAutY"  # Your bot token
+BOT_TOKEN = "6126511065:AAHLPF8CuwowgQm9NaYK_vR_caAD_c0tCxg"  # Your bot token
 
-ERROR_ID = -1001773407390 #private -- Blue Errors log
-SUPPORT_CHAT = "JHBots"
-SUPPORT_ID = -1001773407390 #devslab
+ERROR_ID = -1001905486162 #private -- Blue Errors log
+SUPPORT_CHAT = "NanosTestingArea"
+SUPPORT_ID = -1001905486162 # Nanos Test Group
 
 DOWNLOAD_DIRECTORY = "./"
 
 #DB_URI = "postgres://cugocwks:jgpqMTLw2rO6KMwnWDL6kAXwmaVMB1qW@john.db.elephantsql.com/cugocwks"
-MONGO_DB = "mongodb+srv://ishikki:ishikki143@userinfo.qud86ys.mongodb.net/?retryWrites=true&w=majority"
-MONGO_DB1 = "mongodb+srv://SpyNano:SpyNano@waifubot.igxevb6.mongodb.net/?retryWrites=true&w=majority"
+MONGO_DB = "mongodb+srv://jarvis:op@cluster0.7tisvwv.mongodb.net/?retryWrites=true&w=majority"
+MONGO_DB1 = "mongodb+srv://vortex:yNNrzMsR0BAiI4iY@cluster0.8sizo.mongodb.net/?retryWrites=true&w=majority"
 
 OWNER_ID = [6198858059]
 
